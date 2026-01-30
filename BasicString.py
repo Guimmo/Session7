5f0c2183627908c9e4e6314185399abd47d28e12
guill <guillesfx481@gmail.com>
2026-01-30 11:48:29 +0100

    String example no1

---
+++ BasicString.py
@@ -0,0 +1,16 @@
+text = "Hello World"
+print(text)
+text = 'Hello World 2' #single vs double
+print(text)
+print(text[4])
+print(len(text)) #text length
+text = ""
+print(len(text))
+
+text = "Bob"
+print(text[-1])  #last character in string
+print(text[-3])
+
+#print(text[3]) # this is an error
+print(text[6//3])
+
